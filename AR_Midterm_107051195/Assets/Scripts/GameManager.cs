a8c7d0fe2bbf072cc963d9268cb1008b78e942de
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2020-11-14 17:33:00 +0800

    遊戲管理器

---
+++ AR_Midterm_107051195/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("角色A")]
+    public Transform presonA;
+    [Header("角色A")]
+    public Transform presonB;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+
+    private void Start()
+    {
+        print("開始事件");
+    }
+
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+    }
+}
